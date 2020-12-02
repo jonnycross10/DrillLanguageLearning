92910189f70daaa2f6e3a8e1d8d8ab051e040893
jonnycross10 <crossjonny5@gmail.com>
2020-12-02 05:41:54 -0800

    program will now correctly add english and japanese to mp3 file

--- quizlet.py
+++ quizlet.py
@@ -16,9 +16,16 @@ for i in newText:
 
 print(d)
 
-
+'''
 language = 'ja'
 
 myObj = gTTS(text = txt, lang = language, slow=False)
 
 myObj.save("txt.mp3")
+'''
+with open('text.mp3','wb') as fp:
+	for i in d:
+		eng = gTTS(i,lang='en')
+		jap =  gTTS(d[i], lang='ja')
+		eng.write_to_fp(fp)
+		jap.write_to_fp(fp)
