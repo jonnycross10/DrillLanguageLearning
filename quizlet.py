da6d7d5444e26b94b6b608ddaf9d242571a748b8
jonnycross10 <crossjonny5@gmail.com>
2020-12-03 23:57:51 -0800

    Update quizlet.py

--- quizlet.py
+++ quizlet.py
@@ -2,9 +2,47 @@ import requests
 from gtts import gTTS
 import os
 
-txt = '''flower	お花
-waterfall	滝
-car	車'''
+txt = '''good	いい
+early	はやい
+new	あたらしい
+hot weather	あつい
+busy	いそがしい
+large	おおきい
+interesting	おもしろい
+frightening	こわい
+cold weather	さむい
+fun	たのしい
+small	ちいさい
+boring	つまらない
+old	ふるい
+difficult	むずかしい
+easy	やさしい
+cheap	やすい
+expensive	たかい
+dislike	きらいな
+clean	きれいな
+healthy	げんきな
+quiet	しずかな
+fond of	すきな
+to hate	だいきらいな
+to love	だいすきな
+lively	にぎやかな
+handsome	ハンサムな
+not busy	ひまな
+tough	たいへんな
+smart	あたまがいい
+great looking	かっこいい
+cute	かわいい
+tall	せがたかい
+short stature	せがひくい
+long	ながい
+fast	はやい
+short length	みじかい
+kind	しんせつな
+convenient	べんりな
+skillful	じょうずな
+clumsy	へたな
+famous	ゆうめいな'''
 
 newText = txt.split("\n")
 d = {}
@@ -25,7 +63,8 @@ myObj.save("txt.mp3")
 '''
 with open('text.mp3','wb') as fp:
 	for i in d:
-		eng = gTTS(i,lang='en')
-		jap =  gTTS(d[i], lang='ja')
+		eng = gTTS(i,lang='en',slow=True)
+		jap =  gTTS(d[i], lang='ja',slow=True)
 		eng.write_to_fp(fp)
+		gTTS("3, 2, 1", lang = 'en', slow=True).write_to_fp(fp)
 		jap.write_to_fp(fp)
