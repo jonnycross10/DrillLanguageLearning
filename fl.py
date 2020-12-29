5ba3f85d2ba38b6d578f46ed8db0b67613057a2b
jonnycross10 <crossjonny5@gmail.com>
2020-12-29 00:12:55 -0800

    Update fl.py

--- fl.py
+++ fl.py
@@ -1,11 +1,20 @@
 from flask import Flask
-from flask import send_file
+from flask import send_file, render_template, request
+from quizlet import quizletmp3
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST','GET'])
+@app.route("/", methods=['GET'])
+def index():
+	return render_template('index.html')
+
+@app.route("/mp3", methods=['POST','GET'])
 def mp3():
-	return None
+	text = request.form['text']
+	print(text)
+	quizletmp3(text)
+	return send_file("text.mp3",as_attachment=True)
+
 
 
 if __name__ == '__main__':
