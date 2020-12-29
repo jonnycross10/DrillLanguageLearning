322b642ec2e7528d07488f9ad5d10449772882c4
jonnycross10 <crossjonny5@gmail.com>
2020-12-29 00:17:53 -0800

    Update quizlet.py

--- quizlet.py
+++ quizlet.py
@@ -2,69 +2,28 @@ import requests
 from gtts import gTTS
 import os
 
-txt = '''good	いい
-early	はやい
-new	あたらしい
-hot weather	あつい
-busy	いそがしい
-large	おおきい
-interesting	おもしろい
-frightening	こわい
-cold weather	さむい
-fun	たのしい
-small	ちいさい
-boring	つまらない
-old	ふるい
-difficult	むずかしい
-easy	やさしい
-cheap	やすい
-expensive	たかい
-dislike	きらいな
-clean	きれいな
-healthy	げんきな
-quiet	しずかな
-fond of	すきな
-to hate	だいきらいな
-to love	だいすきな
-lively	にぎやかな
-handsome	ハンサムな
-not busy	ひまな
-tough	たいへんな
-smart	あたまがいい
-great looking	かっこいい
-cute	かわいい
-tall	せがたかい
-short stature	せがひくい
-long	ながい
-fast	はやい
-short length	みじかい
-kind	しんせつな
-convenient	べんりな
-skillful	じょうずな
-clumsy	へたな
-famous	ゆうめいな'''
+txt = '''flower	お花
+waterfall	滝
+car	車'''
 
-newText = txt.split("\n")
-d = {}
+def quizletmp3(text):
+	newText = text.split("\n")
+	d = {}
 
-for i in newText:
+	for i in newText:
 
-  keyVal = i.split("\t")
-  d[keyVal[0]] = keyVal[1]
+	  keyVal = i.split("\t")
+	  d[keyVal[0]] = keyVal[1]
 
-print(d)
+	print(d)
 
-'''
-language = 'ja'
+	with open('text.mp3','wb') as fp:
+		for i in d:
+			eng = gTTS(i,lang='en',slow=False)
+			jap =  gTTS(d[i], lang='ja',slow=True)
 
-myObj = gTTS(text = txt, lang = language, slow=False)
+			eng.write_to_fp(fp)
+			gTTS("3, 2, 1", lang = 'en', slow=False).write_to_fp(fp)
+			jap.write_to_fp(fp)
 
-myObj.save("txt.mp3")
-'''
-with open('text.mp3','wb') as fp:
-	for i in d:
-		eng = gTTS(i,lang='en',slow=True)
-		jap =  gTTS(d[i], lang='ja',slow=True)
-		eng.write_to_fp(fp)
-		gTTS("3, 2, 1", lang = 'en', slow=True).write_to_fp(fp)
-		jap.write_to_fp(fp)
+quizletmp3(txt)
